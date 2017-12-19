783d1493aacfc83d8a99a516384f4142ff1e68f0
jiangzeping <1092116709@qq.com>
2017-12-19 23:44:02 +0800

    first commit

---
+++ shiyan/server/db.js
@@ -0,0 +1,13 @@
+/**
+ * Created by xiaoze on 2017/12/5.
+ */
+// 数据库连接配置
+module.exports = {
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'test',
+    port: '3306'
+  }
+}
---
+++ shiyan/server/sqlMap.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xiaoze on 2017/12/5.
+ */
+// sql语句
+var sqlMap = {
+  // 用户
+  user: {
+    add: 'insert into user(id, name, age) values (0, ?, ?)'
+  }
+}
+module.exports = sqlMap
